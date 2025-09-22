fdc983441a52fc7e1d132a69f71d35dbfb87f3b1
Rerisson H R Silva <148692810+RerissonHRS@users.noreply.github.com>
2025-09-22 13:32:32 -0300

    Update and rename Hellocontroller.java.txt to Hellocontroller.java

---
+++ SpringMVCApp/src/main/java/com/example/controller/Hellocontroller.java
@@ -0,0 +1,16 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public ModelAndView hello() {
+        ModelAndView mv = new ModelAndView("hello");
+        mv.addObject("message", "Olá, Spring MVC com Tomcat 8 rodando no NetBeans!");
+        return mv;
+    }
+}
